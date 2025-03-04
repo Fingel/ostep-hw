a623f55b2f591e25067b46173d66576410eee9e5
Austin Riba <austin@m51.io>
2025-03-04 13:48:18 -0800

    Usage

--- ch-13-address-spaces/src/main.rs
+++ ch-13-address-spaces/src/main.rs
@@ -1,9 +1,14 @@
 use std::env;
 use std::mem::size_of;
+use std::process::exit;
 use std::thread::sleep;
 use std::time::Duration;
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <megabytes>", args[0]);
+        exit(1);
+    }
     let megabytes = &args[1].parse::<usize>().unwrap();
     let buf = vec![255u8; 1024 * 1024 * megabytes];
     dbg!(megabytes);
